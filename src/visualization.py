06ceef9cf29db64bf5fa5cd9d7cfe6d262e162d9
ZachEichen <Zachary.eichen@gmail.com>
2023-12-06 13:38:24 -0500

    added better interface for visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,7 +1,9 @@
 import torch 
+import sys
 from utils import * 
 import subprocess
-from ENV import FFMPEG_PATH
+from os.path import join
+import os 
 import logging 
 torchvision.disable_beta_transforms_warning()
 
@@ -9,14 +11,28 @@ from dataset.vot_dataset import *
 from peripheral_foveal_vision_model import PeripheralFovealVisionModel
 
 
-def visualize_video(model, video_frames,gt_bounding_box,normby=None):
+def visualize_video(model, video_frames,gt_bounding_box,targ_dir=None,draw_bbox=True):
     model.eval()
     with torch.no_grad(): 
         bboxes, fixations = model(video_frames)
     
-    imgs_with_bboxes = draw_bboxes(video_frames,[bboxes,fixations,gt_bounding_box],names=['predicted bb','fovea','ground truth bb'],norm_by=normby)
+    to_dir = 'out'
+    if targ_dir is not None: 
+        to_dir = join('out',targ_dir)
+        os.makedirs(to_dir,exist_ok=True)
+
+    if draw_bbox: 
+        names = ['predicted-bb','fovea','ground truth bb']
+        bboxes = [bboxes,fixations,gt_bounding_box]
+        normby = ['default','default','xyxy']
+    else: 
+        names = ['fovea','ground truth bb']
+        bboxes = [fixations,gt_bounding_box]
+        normby = ['default','xyxy']
+
+    imgs_with_bboxes = draw_bboxes(video_frames,bboxes,names=names,norm_by=normby)
     for i in range(video_frames.shape[0]): 
-        torchvision.io.write_png(imgs_with_bboxes[i,...],f'out/bbox_{i}.png')
+        torchvision.io.write_jpeg(imgs_with_bboxes[i,...],join(to_dir,f'bbox_{i}.jpg'))
     # this assumes you have ffmpeg on your system and added to your path 
     # to install it you can run the following commands: (in an appropriate directory )
     # this downloads a precompiled binary (linked to from the official ffmpeg project page) unpacks it and adds it to your path
@@ -26,16 +42,44 @@ def visualize_video(model, video_frames,gt_bounding_box,normby=None):
     #       chmod +x ffmpeg-6.1-amd64-static/ffmpeg
     #       echo "export PATH=$(pwd)/ffmpeg-6.1-amd64-static:\$PATH" >>~/.bashrc
     #       source ~/.bashrc
+    #       set FFMPEG_PATH in ENV.py to your actual path value 
+    try: 
+        from ENV import FFMPEG_PATH
 
-    subprocess.run([FFMPEG_PATH,'-y','-f','image2','-i','out/bbox_%d.png', 'out/bboxes.mp4'])
+        subprocess.run([FFMPEG_PATH,'-y','-f','image2','-i',join(to_dir,'bbox_%d.jpg'), join(to_dir,'bboxes.mp4')])
+    except ImportError:
+        print('skipping video production')
 
 
-def main(model_path,choose_vid=32): 
+def main(model_path,targdir='sample_{}',choose_vids=[9,567,293,104,871,32,712,255, 2 ,15, 74, 204,314,370],device='cpu'):
+
     model = PeripheralFovealVisionModel()
-    model.load_state_dict(torch.load(model_path))
-    ds = VotDataset()
+    model.load_state_dict(torch.load(model_path,map_location=torch.device(device)))
+    # _, test_loader = get_train_test_dataloaders(seed=1,batch_size=3,targ_size=(224, 224),clip_length_s=5)
+    ds = NonNullVotDataset()
+    # ds = test_loader.dataset
+    print(len(ds))
     # print(ds[choose_vid][1])
-    visualize_video(model,ds[choose_vid][0], ds[choose_vid][1],normby = ['default','default','xyxy'])
+    for choose_id in choose_vids: 
+        targdir_fmt = targdir.format(choose_id)
+        vid, label = ds[choose_id]
+        visualize_video(model,vid, label,targ_dir=targdir_fmt,draw_bbox=True)
 
 if __name__ == "__main__":
-    main('/home/zeichen/foveated_vision/models/20231205_163401_model_epoch_1_step_100.pth')
+    basedir = '/scratch/eecs542s001f23_class_root/eecs542s001f23_class/shared_data/group_raz/models/'
+    name = '20231206_072942_model_epoch_2_step_2080.pth' 
+
+    # basedir = 'models'
+    # name = '20231206_051901_model_epoch_1_step_1360_loss_0.0013735051034018397.pth'
+    if len(sys.argv) ==3: 
+        basedir = sys.argv[1]
+        name = sys.argv[2]
+    elif len(sys.argv) == 2: 
+        name = sys.argv[1]
+
+    print(f'\n\n\n USING MODEL {name}')
+    targdir='mine_01_{}'
+    to_vis = [2, 9, 15, 373, 396, 456, 562, 567, 581, 637, 651, 710, 742, 760, 910, 990]
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    main(join(basedir,name),targdir = targdir,choose_vids=to_vis,device=device)
+    print(f'USING MODEL {name}\n\n\n ')
